a730706d96c095c1f4cba666f5e855bc8a4d1ed7
sulmi24 <a.sulmi24@gmail.com>
2023-03-21 18:15:18 +0200

    Solved : Assignment Done

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ColorItem from "./ColorItem ";
 
 function ColorList() {
   const colors = [
@@ -9,16 +10,37 @@ function ColorList() {
     "hotpink",
   ];
 
+  // const colorElements = colors.map((color) => {
+  //   return <li style={{ color: color }}>{color}</li>;
+  // });
+
+  const users = [
+    { id: 1, firstName: "Duane", lastName: "Watson" },
+    { id: 2, firstName: "Duane", lastName: "Johnson" },
+  ];
+
+  const userHeadings = users.map((user) => {
+    return <h1 key={user.id}>{user.firstName}</h1>;
+  });
+
+  const colorElements = colors.map((color) => {
+    return <ColorItem key={color} color={color} />;
+  });
+
+  const fib = [0, 1, 1, 2, 3, 5];
+
+  const fibList = fib.map((number, index) => {
+    return <div key={index}>{number}</div>;
+  });
+
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
-      <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
-      </ol>
+      <ol>{colorElements}</ol>
+      <h1>Top Users</h1>
+      <ol>{userHeadings}</ol>
+      <h1>Fib List</h1>
+      <ol>{fibList}</ol>
     </div>
   );
 }
